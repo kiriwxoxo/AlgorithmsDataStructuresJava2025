0b1f667b71ef6b7013e0a787a2d2d352fbff2493
kiriwxoxo <kateryna.verkhovetska@stud.onu.edu.ua>
2025-11-24 18:19:46 +0300

    Add mergeTwoLists method for merging linked lists
    Implement mergeTwoLists method to merge two sorted linked lists.

--- LinkedList/Solution.java
+++ LinkedList/Solution.java
@@ -1,1 +1,34 @@
-jj
+class Solution {
+    //принимает2 начала списков и возвращает началосписка
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        ListNode temporarily = new ListNode(0);
+        
+        // current указатель ("курсор") cначала он указывает на временную голову.
+        ListNode current = temporarily;
+
+        // Цикл работает пока в ОБОИХ списках еще есть элементы
+        while (list1 != null && list2 != null) {
+            
+            if (list1.val < list2.val) {
+                current.next = list1;
+                // сдвигаем ук лист1 на 1 вперед
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            // сдвигаем наш курсор на узел,
+            current = current.next;
+        }
+
+
+        if (list1 != null) {
+            current.next = list1;
+        } else {
+            current.next = list2;
+        }
+
+        return temporarily.next;
+    }
+}
