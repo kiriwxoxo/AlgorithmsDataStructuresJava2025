8955e790c0139417b2ac0475cf6bec1fbac35224
kiriwxoxo <kateryna.verkhovetska@stud.onu.edu.ua>
2025-11-24 18:19:23 +0300

    Implement ListNode class for linked list

--- LinkedList/ListNode.java
+++ LinkedList/ListNode.java
@@ -1,1 +1,10 @@
-ss
+public class ListNode { 
+    int val;            
+    ListNode next;      
+
+    ListNode() {}       
+
+    ListNode(int val) { this.val = val; }
+
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
