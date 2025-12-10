3442c2688db09e76bae352cd71e454ac1e6c680f
kiriwxoxo <kateryna.verkhovetska@stud.onu.edu.ua@>
2025-12-10 19:41:28 +0300

    commit

---
+++ Eolymp/src/scr/Main.java
@@ -0,0 +1,17 @@
+package scr;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = 0;
+        if (x<5){
+            y = x*x - 3*x + 4;
+        }
+        else {
+            y = x+7;
+        }
+        System.out.println(y);
+    }
+}
